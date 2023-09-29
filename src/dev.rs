64143f40b05f1de0551f0d113bb36ce955be1a1d
djedou <djedouarnaud.aman@gmail.com>
2023-09-29 17:55:23 +0200

    update dev

--- src/dev.rs
+++ src/dev.rs
@@ -2,7 +2,7 @@ use std::process::{Command, Stdio};
 
 pub(crate) fn dev() {
     match Command::new("cargo")
-            .args(["watch", "-w", "src/app", "-s", ".\\tools\\binuid-dev.exe & wasm-pack build --target web --release"])
+            .args(["watch", "-w", "src", "-s", ".\\tools\\binuid-dev.exe & wasm-pack build --target web --release"])
             .stdout(Stdio::inherit())
             .status()
         {
