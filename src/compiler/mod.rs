07bdcbb781a75fedf0d843377374cbde52e01e00
djedou <djedouarnaud.aman@gmail.com>
2024-10-15 07:29:11 +0200

    llvm

--- src/compiler/mod.rs
+++ src/compiler/mod.rs
@@ -1,12 +1,15 @@
+mod file;
+
 use std::path::PathBuf;
 use std::{env, process::{Command, Stdio}};
 use binuid_shared_wasm::console::{info, error};
-use binuid_shared::syn;
+use binuid_shared::syn::{self, visit::Visit};
 use std::{
     fs::File,
     io::Read
 };
 use crate::{get_duid_bin_build, get_duid_bin_lib_build, Result};
+use file::BinuidFile;
 
 pub struct Compiler {
     pub name: String, 
@@ -31,14 +34,15 @@ impl Compiler {
     pub fn compile(&self) -> Result<()> {
         info!("compiling...");
         self.compile_lib_bin()?;
-        let mut errors_count = 0; 
+        let mut errors_count = 0;
+        let libs_ids = self.get_libs_paths();
         for file in &self.files {
             match (
                 file.as_path().extension().map_or(None, |m| m.to_str()),
                 file.as_path().to_str().map_or(false, |d| d.contains("pages"))
             ) {
                 (Some("rs"), true) => {
-                    match self.compile_file(&file) {
+                    match self.compile_file(&file, &libs_ids) {
                         Ok(_) => {},
                         Err(e) => {
                             return Err(e);
@@ -52,14 +56,17 @@ impl Compiler {
         Ok(())
     }
 
-    pub fn compile_file(&self, path: &PathBuf) -> Result<()> {
+    pub fn compile_file(&self, path: &PathBuf, _libs_ids: &[(String, String)]) -> Result<()> {
         let agrs = get_duid_bin_build(path.to_str(), &self.lib_dependencies);
         Command::new("rustc").args(&agrs).stdout(Stdio::inherit()).status()?;
+        
+        /*
         let mut file = File::open(path)?;
         let mut content = String::new();
         file.read_to_string(&mut content)?;
         let ast = syn::parse_file(&content)?;
-        println!("ast: {:#?}", ast.items.len());
+        let mut build_file = BinuidFile::new();
+        build_file.load(&ast);*/
 
         Ok(())
     }
@@ -68,5 +75,20 @@ impl Compiler {
         let agrs = get_duid_bin_lib_build(&self.name, &self.version, &self.lib_dependencies);
         Command::new("rustc").args(&agrs).stdout(Stdio::inherit()).status()?;
         Ok(())
+    }
+
+    pub fn get_libs_paths(&self) -> Vec<(String, String)> {
+        let mut names = vec![];
+        self.lib_dependencies.iter().for_each(|d| {
+            match d.contains("=") {
+                true => {
+                    let splited: Vec<_> = d.split("=").collect();
+                    names.push((splited[0].to_string(), splited[1].to_string()));
+                },
+                false => {}
+            }
+        });
+
+        names
     }
 }
--- src/config/builders.rs
+++ src/config/builders.rs
@@ -8,9 +8,11 @@ pub(crate) fn get_duid_lib_build(name: &str, version: &str, deps: &[String]) -> 
     current_dir.push("lib");
     current_dir.push(format!("{new_name}.rs"));
     let mut args = vec![
+        "-O".to_string(),
         "--crate-type=lib".to_string(),
         format!("--crate-name={new_name}_v_{version}"),
-        "--edition=2021".to_string()
+        "--edition=2021".to_string(),
+        "--emit=link,llvm-ir".to_string()
     ];
     args.extend_from_slice(&deps);
     args.push(format!("{}", current_dir.display()));
@@ -27,10 +29,12 @@ pub(crate) fn get_duid_bin_lib_build(name: &str, version: &str, deps: &[String])
     current_dir.push("lib");
     current_dir.push("mod.rs");
     let mut args = vec![
+        "-O".to_string(),
         "--crate-type=lib".to_string(),
         format!("--crate-name={name}_v_{version}"),
         format!("--out-dir={}", output_directory.display()),
-        "--edition=2021".to_string()
+        "--edition=2021".to_string(),
+        "--emit=link,llvm-ir".to_string()
     ];
     args.extend_from_slice(&deps);
     args.push(format!("{}", current_dir.display()));
@@ -63,10 +67,12 @@ pub(crate) fn get_duid_bin_build(path: Option<&str>, deps: &[String]) -> Vec<Str
         }
     });
     let mut args = vec![
+        "-O".to_string(),
         "--crate-type=lib".to_string(),
         format!("--crate-name={new_name}"),
         format!("--out-dir={directory}"),
-        "--edition=2021".to_string()
+        "--edition=2021".to_string(),
+        "--emit=link,llvm-ir".to_string()
     ];
     args.extend_from_slice(&deps);
     args.push(format!("{}", path.map_or("", |p| p)));
